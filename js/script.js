6f00b8cc36a797cafe7e82b9d5ca59d37e6e233e
BibiaSF <ana.b.freitas8@aluno.senai.br>
2024-10-31 11:04:23 -0300

    aula_31/10_pag_perfil

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
 const form = document.getElementById('form');
-const email_input = document.getElementById('email-input')
-const password_input = document.getElementById('password-input')
-const error_message = document.getElementById('error-message')
+const name_input = document.getElementById('name-input');
+const email_input = document.getElementById('email-input');
+const password_input = document.getElementById('password-input');
+const repeat_password_input = document.getElementById('repeat-password-input');
+const error_message = document.getElementById('error-message');
 
 form.addEventListener('submit' , (e) => {
     e.preventDefault();
     let errors = [];
 
-    errors = validateLoginForm(
-        email_input.value,
-        password_input.value
-    );
+    const fileName = e.target.baseURI.split("/").pop();
 
-    //Verificar Login
-    if (errors.length === 0) {
-       const isValidLogin = verifyLogin(
-       email_input.value,
-       password_input.value
-       );
-       if (!isValidLogin){
-        errors.push('Email ou Senha incorretos');
-       }
+    if (fileName === "register.html") {
+        errors = validateRegisterForm(
+            name_input.value,
+            email_input.value,
+            password_input.value,
+            repeat_password_input.value
+        );
+    } else{
+        errors = validateLoginForm(
+            email_input.value,
+            password_input.value
+        );
+    
+        //Verificar Login
+        if (errors.length === 0) {
+           const isValidLogin = verifyLogin(
+           email_input.value,
+           password_input.value
+           );
+           if (!isValidLogin){
+            errors.push('Email ou Senha incorretos');
+           }
+        }
     }
 
     if (errors.length > 0){
@@ -28,7 +41,7 @@ form.addEventListener('submit' , (e) => {
         return
     }
 
-    window.location.href = "home.html";
+    window.location.href = "home.html"
 });
 
 function validateLoginForm(email,password) {
@@ -50,6 +63,8 @@ function validateLoginForm(email,password) {
 const allInputs = [
     email_input,
     password_input,
+    name_input,
+    repeat_password_input
   ].filter((input) => input != null);
   
   allInputs.forEach((input) => {
@@ -84,3 +99,40 @@ function verifyLogin(email, password) {
         return false;
     }
 }
+
+function validateRegisterForm(name, email, password, repeatPassword) {
+    let errors = [];
+  
+    if (name === "" || name == null) {
+      errors.push("Nome é obrigatório");
+      name_input.parentElement.classList.add("incorrect");
+    }
+  
+    if (email === "" || email == null) {
+      errors.push("Email é obrigatório");
+      email_input.parentElement.classList.add("incorrect");
+    }
+  
+    if (password === "" || password == null) {
+      errors.push("Senha é obrigatória");
+      password_input.parentElement.classList.add("incorrect");
+    } else {
+      if (password.length < 8) {
+        errors.push("Senha deve ter no mínimo 8 caracteres");
+        password_input.parentElement.classList.add("incorrect");
+      }
+    }
+  
+    if (repeatPassword === "" || repeatPassword == null) {
+      errors.push("Repita a senha");
+      repeat_password_input.parentElement.classList.add("incorrect");
+    } else {
+      if (password !== repeatPassword) {
+        errors.push("Senhas não conferem");
+        password_input.parentElement.classList.add("incorrect");
+        repeat_password_input.parentElement.classList.add("incorrect");
+      }
+    }
+  
+    return errors;
+  }
